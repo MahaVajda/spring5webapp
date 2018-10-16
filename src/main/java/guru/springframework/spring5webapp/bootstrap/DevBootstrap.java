c800dc2ea1b0bfeaa96f39e1303622afcdc63339
Martin Vajda <vajda_martin@vseholding.sk>
2018-10-16 14:58:46 +0200

    Pridanie Publishera oneToOne.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -6,34 +6,45 @@ import org.springframework.stereotype.Component;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
-	
-	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
-		super();
-		this.authorRepository = authorRepository;
-		this.bookRepository = bookRepository;
-	}
+
 
 	private AuthorRepository authorRepository;
 	private BookRepository bookRepository;
+	private PublisherRepository publisherRepository;
 	
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository,
+			PublisherRepository publisherRepository) {
+		super();
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+
 	private void initData() {
+		Publisher rockStar = new Publisher();
+		rockStar.setAddress("Juzna trieda 9");
+		rockStar.setName("RockStar");
+		
+		publisherRepository.save(rockStar);
 		
 		Author adam = new Author("Adam", "Maly");
-		Book adamKniha = new Book("Mala kniha","12345", "RockStarPublisher");
+		Book adamKniha = new Book("Mala kniha","12345",  rockStar);
 		
 		adam.getBooks().add(adamKniha);
 		adamKniha.getAuthorses().add(adam);
 		
 		authorRepository.save(adam);
 		bookRepository.save(adamKniha);
-		
+
 		Author jozef = new Author("Jozef", "Mak");
-		Book knihaJozko = new Book("Jozkova prva","789","ActivisionPublisher");
+		Book knihaJozko = new Book("Jozkova prva","789", rockStar);
 		
 		jozef.getBooks().add(knihaJozko);
 		
